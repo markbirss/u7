caf86e46034adfaf6d0f2e70469faa4653011e64
Mark Birss <markbirss@gmail.com>
2026-04-11 11:37:13 +0200

    Add u7 Board sdk

---
+++ buildroot/board/rockchip/rk3506/fast-display-overlay/etc/init.d/S03modules_init.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Modules init
+#
+
+
+start() {
+	find /lib/modules/$(uname -r)/kernel/ -name "*.ko" | xargs -I {} sh -c 'modprobe $(basename {})'
+}
+
+stop() {
+	#find /lib/modules/$(uname -r)/kernel/ -name "*.ko" | xargs -I {} sh -c 'modprobe -r $(basename {})'
+	echo "not have stop function!"
+}
+
+case "$1" in
+	start)
+		start
+		;;
+	stop)
+		stop
+		;;
+	*)
+		echo "Usage: $0 {start|stop|restart}"
+		exit 1
+esac
+
+exit $?
